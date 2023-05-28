7daa98e0a81eb7f99c9543436cbba8e5352ba88b
Shekhar Shrestha <shekharr.shresthaa@gmail.com>
2023-05-28 14:16:03 +0545

    fix: big header, spacing, redesigned why us, carouser loop

--- js/index.js
+++ js/index.js
@@ -1,39 +1,40 @@
 $('.multiple-slides').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    prevArrow: '<i class="fa-solid fa-chevron-left left-arrow"></i>',
-    nextArrow: '<i class="fa-solid fa-chevron-right right-arrow">',
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
+  dots: true,
+  infinite: true,
+  loop: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow: '<i class="fa-solid fa-chevron-left left-arrow"></i>',
+  nextArrow: '<i class="fa-solid fa-chevron-right right-arrow"></i>',
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
 
   
 $(".testimonial-carousel").slick({
   infinite: true,
   slidesToShow: 2,
   arrows: false,
+  autoplay: true,
+  speed: 300,
+  loop: true,
   dots: true,
   responsive: [
     {
